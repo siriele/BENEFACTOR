4f99317807a6f4819a7278be11d1d4bd3099581d
Stanley <siriele@siriele-MS-7681.(none)>
2014-03-15 22:36:41 -0700

    Adding naive implemntation of counting distinct subset sums

---
+++ Erlang/sample.erl
@@ -0,0 +1,9 @@
+-module (sample).
+-export ([perm_sum/1]).
+
+perm_sum(1) ->[[1]];
+perm_sum(N) -> 
+	Head = [N],
+	Seq=[{S, lists:filter(fun(X)-> lists:max(X) =< S end ,perm_sum(N-S))} || S <-lists:reverse(lists:seq(1,N-1))],
+	Result=lists:foldr(fun ({Key,PermsList}, Acc1) -> lists:foldr(fun(P, Acc)-> [[Key|P]|Acc] end, Acc1, PermsList) end, [], Seq),
+	[Head|Result].
