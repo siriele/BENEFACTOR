0af0ce089ec58c0d84a8f58b2bf1a580da7cfdee
siriele <siriele2x3@gmail.com>
2014-03-16 13:50:23 -0700

    forgot the guard was not placed in this file

--- Erlang/sample.erl
+++ Erlang/sample.erl
@@ -2,7 +2,7 @@
 -export ([perm_sum/1]).
 
 perm_sum(1) ->[[1]];
-perm_sum(N) -> 
+perm_sum(N) when N > 1-> 
 	Head = [N],
 	Seq=[{S, lists:filter(fun(X)-> lists:max(X) =< S end ,perm_sum(N-S))} || S <-lists:reverse(lists:seq(1,N-1))],
 	Result=lists:foldr(fun ({Key,PermsList}, Acc1) -> lists:foldr(fun(P, Acc)-> [[Key|P]|Acc] end, Acc1, PermsList) end, [], Seq),
